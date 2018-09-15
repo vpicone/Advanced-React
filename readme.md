26667c94b177c56ddcda55c8ce99b467d6afd4b5
Vincent Picone <Vincent.Patrick.Picone@ibm.com>
2018-09-15 12:44:17 -0500

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Advanced React + GraphQL
+
+Coming Soon!
+
+
+
+## Early Access Bugs Already Fixed
+
+* [X] Upgrade Next.js
+* [x] Video 7 reset at 0:45
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <p>Sell</p>
+  </div>
+);
+
+export default Home;
